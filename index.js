715268d158aac741fa26c1d9c48f9f7e791cf77f
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-02-17 03:03:03 +0600

    implemented post and get api

--- index.js
+++ index.js
@@ -26,7 +26,27 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    //db-connections
+    const credentialsCollection = client
+      .db("Credentials-of-Anas")
+      .collection("Credentials");
+
+      //getting all credentials
+      app.get("/credentials", async (req, res) => {
+          const result = await credentialsCollection.find().toArray();
+          res.send(result)
+      })
+      
+
+    // storing all of credentials
+    app.post("/credentials", async (req, res) => {
+      const credential = req.body;
+      const result = await credentialsCollection.insertOne(credential);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,7 +54,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
